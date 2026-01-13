86340da768c1a16c05ca881ad0dd9770551aefb1
tuanznguyen <redmi15052003@gmail.com>
2026-01-13 21:19:53 +0700

    Add files via upload

---
+++ baitap1.java
@@ -0,0 +1,9 @@
+
+import java.util.Random;
+public class baitap1 {
+    public static void main(String[] args) {
+        Random random = new Random();     // Tạo đối tượng Random
+        int soNgauNhien = random.nextInt(100) + 1;      
+        System.out.println("So nguyen ngau nhien tu 1 den 100 la: " + soNgauNhien);
+    }
+}
